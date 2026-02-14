d3d807bbfd322d3939f80cc419405baa8f4e403c
ani6132457 <ani@clarabel.jp>
2026-02-14 12:42:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ for _, row in rows.iterrows():
                 <div style="
                     font-size:15px;
                     font-weight:600;
-                    margin-top:6px;
+                    margin-top:8px;
                     line-height:1.2;
                 ">
                     販売可能: <span style="color:#007bff;">{available}</span>
